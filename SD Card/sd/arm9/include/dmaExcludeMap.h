1da3b9403116bb27b08e46a75bfc2b8409b31ed2
RocketRobz <bobesh8@gmail.com>
2022-08-09 18:12:16 -0600

    SD Card: Update card read DMA blacklist

--- SD Card/sd/arm9/include/dmaExcludeMap.h
+++ SD Card/sd/arm9/include/dmaExcludeMap.h
@@ -3,11 +3,13 @@
 
 static const char cardReadDMAExcludeList[][4] = {
 	"TAM", // The Amazing Spider-Man
+	"CBX", // Black Sigil: Blade of the Exiled
 	"AWD", // Diddy Kong Racing
 	"BO5", // Golden Sun: Dark Dawn
 	"Y8L", // Golden Sun: Dark Dawn (Demo)
 	"AJS", // Jump! Super Stars
 	"AJU", // Jump! Ultimate Stars
+	"VPY", // Pokémon Conquest
 	"B8I", // Spider-Man: Edge of Time
 	"CTX", // Tropix
 	"CP3", // Viva Pinata
