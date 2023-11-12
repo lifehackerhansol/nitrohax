ac8b1880cb066bbb305a818be176f6c42e5f31aa
RocketRobz <bobesh8@gmail.com>
2023-11-11 23:37:30 -0700

    SD Card: Hide settings incompatible with DSi mode

--- SD Card/sd/arm9/source/perGameSettings.cpp
+++ SD Card/sd/arm9/source/perGameSettings.cpp
@@ -43,12 +43,12 @@ void GameSettings::save() {
 	ini.SetInt("GAMESETTINGS", "WIDESCREEN", widescreen);
 
 	// Ensure the folder exists
-	if(access("sd:/_nds", F_OK) != 0)
-		mkdir("sd:/_nds", 0777);
-	if(access("sd:/_nds/ntr-forwarder", F_OK) != 0)
-		mkdir("sd:/_nds/ntr-forwarder", 0777);
-	if(access("sd:/_nds/ntr-forwarder/gamesettings", F_OK) != 0)
-		mkdir("sd:/_nds/ntr-forwarder/gamesettings", 0777);
+	if(access("/_nds", F_OK) != 0)
+		mkdir("/_nds", 0777);
+	if(access("/_nds/ntr-forwarder", F_OK) != 0)
+		mkdir("/_nds/ntr-forwarder", 0777);
+	if(access("/_nds/ntr-forwarder/gamesettings", F_OK) != 0)
+		mkdir("/_nds/ntr-forwarder/gamesettings", 0777);
 
 	ini.SaveIniFile(iniPath);
 }
@@ -59,17 +59,22 @@ void GameSettings::menu() {
 	u16 held;
 	int cursorPosition = 0;
 	int numOptions = consoleModel == 2 ? 9 : 8;
+	if (!isDSiMode()) {
+		numOptions = 3;
+	}
 	while(1) {
 		consoleClear();
 		iprintf("ntr-forwarder\n\n");
 		iprintf("  Language: %s\n", languageLabels[language + 2]);
 		iprintf("  Region: %s\n", regionLabels[region + 3]);
 		iprintf("  Save Number: %d\n", saveNo);
-		iprintf("  Run in: %s\n", runInLabels[dsiMode + 1]);
-		iprintf("  ARM9 CPU Speed: %s\n", cpuLabels[boostCpu + 1]);
-		iprintf("  VRAM Mode: %s\n", vramLabels[boostVram + 1]);
-		iprintf("  Card Read DMA: %s\n", offOnLabels[cardReadDMA + 1]);
-		iprintf("  Async Card Read: %s\n", offOnLabels[asyncCardRead + 1]);
+		if (isDSiMode()) {
+			iprintf("  Run in: %s\n", runInLabels[dsiMode + 1]);
+			iprintf("  ARM9 CPU Speed: %s\n", cpuLabels[boostCpu + 1]);
+			iprintf("  VRAM Mode: %s\n", vramLabels[boostVram + 1]);
+			iprintf("  Card Read DMA: %s\n", offOnLabels[cardReadDMA + 1]);
+			iprintf("  Async Card Read: %s\n", offOnLabels[asyncCardRead + 1]);
+		}
 		iprintf("  Bootstrap File: %s\n", bootstrapLabels[bootstrapFile + 1]);
 		if(consoleModel == 2)
 			iprintf("  Widescreen: %s\n", widescreenLabels[widescreen + 1]);
@@ -94,90 +99,132 @@ void GameSettings::menu() {
 			if(cursorPosition > numOptions)
 				cursorPosition = 0;
 		} else if(held & (KEY_LEFT | KEY_A)) {
-			switch(cursorPosition) {
-				case 0:
-					language--;
-					if(language < -2) language = 7;
-					break;
-				case 1:
-					region--;
-					if(region < -3) region = 5;
-					break;
-				case 2:
-					saveNo--;
-					if(saveNo < 0) saveNo = 9;
-					break;
-				case 3:
-					dsiMode--;
-					if(dsiMode < -1) dsiMode = 2;
-					break;
-				case 4:
-					boostCpu--;
-					if(boostCpu < -1) boostCpu = 1;
-					break;
-				case 5:
-					boostVram--;
-					if(boostVram < -1) boostVram = 1;
-					break;
-				case 6:
-					cardReadDMA--;
-					if(cardReadDMA < -1) cardReadDMA = 1;
-					break;
-				case 7:
-					asyncCardRead--;
-					if(asyncCardRead < -1) asyncCardRead = 1;
-					break;
-				case 8:
-					bootstrapFile--;
-					if(bootstrapFile < -1) bootstrapFile = 1;
-					break;
-				case 9:
-					widescreen--;
-					if(widescreen < -1) widescreen = 2;
-					break;
+			if (isDSiMode()) {
+				switch(cursorPosition) {
+					case 0:
+						language--;
+						if(language < -2) language = 7;
+						break;
+					case 1:
+						region--;
+						if(region < -3) region = 5;
+						break;
+					case 2:
+						saveNo--;
+						if(saveNo < 0) saveNo = 9;
+						break;
+					case 3:
+						dsiMode--;
+						if(dsiMode < -1) dsiMode = 2;
+						break;
+					case 4:
+						boostCpu--;
+						if(boostCpu < -1) boostCpu = 1;
+						break;
+					case 5:
+						boostVram--;
+						if(boostVram < -1) boostVram = 1;
+						break;
+					case 6:
+						cardReadDMA--;
+						if(cardReadDMA < -1) cardReadDMA = 1;
+						break;
+					case 7:
+						asyncCardRead--;
+						if(asyncCardRead < -1) asyncCardRead = 1;
+						break;
+					case 8:
+						bootstrapFile--;
+						if(bootstrapFile < -1) bootstrapFile = 1;
+						break;
+					case 9:
+						widescreen--;
+						if(widescreen < -1) widescreen = 2;
+						break;
+				}
+			} else {
+				switch(cursorPosition) {
+					case 0:
+						language--;
+						if(language < -2) language = 7;
+						break;
+					case 1:
+						region--;
+						if(region < -3) region = 5;
+						break;
+					case 2:
+						saveNo--;
+						if(saveNo < 0) saveNo = 9;
+						break;
+					case 3:
+						bootstrapFile--;
+						if(bootstrapFile < -1) bootstrapFile = 1;
+						break;
+				}
 			}
 		} else if(held & KEY_RIGHT) {
-			switch(cursorPosition) {
-				case 0:
-					language++;
-					if(language > 7) language = -2;
-					break;
-				case 1:
-					region++;
-					if(region > 5) region = -3;
-					break;
-				case 2:
-					saveNo++;
-					if(saveNo > 9) saveNo = 0;
-					break;
-				case 3:
-					dsiMode++;
-					if(dsiMode > 2) dsiMode = -1;
-					break;
-				case 4:
-					boostCpu++;
-					if(boostCpu > 1) boostCpu = -1;
-					break;
-				case 5:
-					boostVram++;
-					if(boostVram > 1) boostVram = -1;
-					break;
-				case 6:
-					cardReadDMA++;
-					if(cardReadDMA > 1) cardReadDMA = -1;
-					break;
-				case 7:
-					asyncCardRead++;
-					if(asyncCardRead > 1) asyncCardRead = -1;
-					break;
-				case 8:
-					bootstrapFile++;
-					if(bootstrapFile > 1) bootstrapFile = -1;
-					break;
-				case 9:
-					widescreen++;
-					if(widescreen > 2) widescreen = -1;
-					break;
+			if (isDSiMode()) {
+				switch(cursorPosition) {
+					case 0:
+						language++;
+						if(language > 7) language = -2;
+						break;
+					case 1:
+						region++;
+						if(region > 5) region = -3;
+						break;
+					case 2:
+						saveNo++;
+						if(saveNo > 9) saveNo = 0;
+						break;
+					case 3:
+						dsiMode++;
+						if(dsiMode > 2) dsiMode = -1;
+						break;
+					case 4:
+						boostCpu++;
+						if(boostCpu > 1) boostCpu = -1;
+						break;
+					case 5:
+						boostVram++;
+						if(boostVram > 1) boostVram = -1;
+						break;
+					case 6:
+						cardReadDMA++;
+						if(cardReadDMA > 1) cardReadDMA = -1;
+						break;
+					case 7:
+						asyncCardRead++;
+						if(asyncCardRead > 1) asyncCardRead = -1;
+						break;
+					case 8:
+						bootstrapFile++;
+						if(bootstrapFile > 1) bootstrapFile = -1;
+						break;
+					case 9:
+						widescreen++;
+						if(widescreen > 2) widescreen = -1;
+						break;
+				}
+			} else {
+				switch(cursorPosition) {
+					case 0:
+						language++;
+						if(language > 7) language = -2;
+						break;
+					case 1:
+						region++;
+						if(region > 5) region = -3;
+						break;
+					case 2:
+						saveNo++;
+						if(saveNo > 9) saveNo = 0;
+						break;
+					case 3:
+						bootstrapFile++;
+						if(bootstrapFile > 1) bootstrapFile = -1;
+						break;
+				}
 			}
 		} else if(held & KEY_B) {
 			return;
